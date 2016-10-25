b48f7f8edd54f41f9c4a70936daa253ab44845df
Aghosh993 <Aghosh993@gmail.com>
2016-10-25 03:18:46 -0400

    Adding current working view of Segway codebase.

--- Rec03_PID/SegwayCode/Segway_Code/Segway_Code.ino
+++ Rec03_PID/SegwayCode/Segway_Code/Segway_Code.ino
@@ -1,17 +1,146 @@
-#include <Wire.h>
 #include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
 #include <Adafruit_LSM9DS0.h>
-#include <Adafruit_Sensor.h>  // not used in this demo but required!
+////////////////////////////////////////////////////////////////////////////////
+/*
+ * Program CONFIGURATION AREA
+ * The options below are BUILD-TIME SETTINGS that control various aspects of the
+ * run-time functionality of this software.
+ */
+////////////////////////////////////////////////////////////////////////////////
 
-// i2c
-Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0();
+//#define RUNTIME_VERBOSE_OUTPUT  1
+#define CONTROL_DT 0.004 // In seconds
 
-// You can also use software SPI
-//Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(13, 12, 11, 10, 9);
-// Or hardware SPI! In this case, only CS pins are passed in
-//Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(10, 9);
+////////////////////////////////////////////////////////////////////////////////
+/*
+ * BEGINNING of supporting functions and declarations:
+ */
+////////////////////////////////////////////////////////////////////////////////
 
-void setupSensor()
+/*
+ * Holds Motor rotation direction in robot global coordinate frame (so has mirroring
+ * of motors due to opposite-facing mounting taken into account):
+ */
+typedef enum {
+  DIR_FWD,
+  DIR_REV
+} motor_dir;
+
+/*
+ * Initialize Timer 1 to FAST PWM, TOP = 512 (i.e. 9-bit PWM) with Output Compare
+ * on both A and B channels:
+ */
+
+void init_timer1_pwm()
+{
+  DDRB |= (1<<1) | (1<<2);
+  TCCR1A = (1<<COM1A1) | (0<<COM1A0) | (1<<COM1B1) | (0<<COM1B0) |
+            (1<<WGM11) | (0<<WGM10);
+  TCCR1B = (0<<WGM13) | (1<<WGM12) | (0<<CS12) | (1<<CS11) | (0<<CS10);
+
+  OCR1A = (uint16_t)128;
+  OCR1B = (uint16_t)256;
+}
+
+void init_timer0_timebase()
+{
+  TCCR2A = (1<<WGM21);  // We don't want any connection to the OC0A or OC0B pins
+                        // CTC mode. Compare on OCR2A
+  TCCR2B = (1<<CS22) | (1<<CS21) | (0<<CS20); // Prescaler=256
+
+  OCR2A = 124;
+
+  TIMSK2 |= (1<<OCIE2A);  
+}
+
+/*
+ * Initializes the DIRECTION control pins as outputs by setting appropriate bits in DDRx
+ * DIR control pins:  PD2, 4 for IN1, IN2 on M1
+ *                    PB4, 5 for IN1, IN2 on M2
+ */
+void setup_motor_control_io()
+{
+  DDRD |= (1<<2) | (1<<4);
+  DDRB |= (1<<4) | (1<<5);
+
+  init_timer1_pwm();
+}
+
+/*
+ * Drives the left-side motor (in robot body coordinate frame) at speed set by "left_motor"
+ * and in direction set by "left_motor_dir"
+ * 
+ * left_motor: An unsigned 16-bit integer from 0 to 511 inclusive
+ * left_motor_dir: An argument of enum "motor_dir"
+ */
+
+void drive_left_motor(uint16_t left_motor, motor_dir left_motor_dir)
+{
+  switch(left_motor_dir)
+  {
+    case DIR_FWD:
+      PORTD |= (1<<2);
+      PORTD &= ~(1<<4);
+      break;
+    case DIR_REV:
+      PORTD &= ~(1<<2);
+      PORTD |= (1<<4);
+      break;
+  }
+  if(left_motor <= 511)
+  {
+    OCR1B = left_motor;
+  }
+}
+
+/*
+ * Drives the right-side motor (in robot body coordinate frame) at speed set by "right_motor"
+ * and in direction set by "right_motor_dir"
+ * 
+ * right_motor: An unsigned 16-bit integer from 0 to 511 inclusive
+ * right_motor_dir: An argument of enum "motor_dir"
+ */
+ 
+void drive_right_motor(uint16_t right_motor, motor_dir right_motor_dir)
+{
+  switch(right_motor_dir)
+  {
+    case DIR_FWD:
+      PORTB &= ~(1<<4);
+      PORTB |= (1<<5);
+      break;
+    case DIR_REV:
+      PORTB |= (1<<4);
+      PORTB &= ~(1<<5);
+      break;
+  }
+  if(right_motor <= 511)
+  {
+   OCR1A = right_motor; 
+  }
+}
+
+void drive_motors(uint16_t left_motor, motor_dir left_motor_dir,
+                  uint16_t right_motor, motor_dir right_motor_dir)
+{
+  drive_left_motor(left_motor, left_motor_dir);
+  drive_right_motor(right_motor, right_motor_dir);  
+}
+
+/*
+ * The IMU Setup Code below is based on the "sensorapi" example from Adafruit's Unified Sensor
+ * API:
+ */
+
+/* Assign a unique base ID for this sensor */   
+Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0(1000);  // Use I2C, ID #1000
+sensors_event_t accel, mag, gyro, temp;
+
+volatile uint8_t get_data;
+
+void configureSensor(void)
 {
   // 1.) Set the accelerometer range
   lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_2G);
@@ -32,38 +161,85 @@ void setupSensor()
   //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_2000DPS);
 }
 
-void setup() 
+int init_imu(void)
 {
-#ifndef ESP8266
-  while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
-#endif
-  Serial.begin(9600);
-  Serial.println("LSM raw read demo");
-  
-  // Try to initialise and warn if we couldn't detect the chip
-  if (!lsm.begin())
+  /* Initialise the sensor */
+  if(!lsm.begin())
   {
-    Serial.println("Oops ... unable to initialize the LSM9DS0. Check your wiring!");
-    while (1);
+    // Error condition... most likely, sensor not on bus!!
+    return -1;    // Houston, we have a problem... :)
   }
-  Serial.println("Found LSM9DS0 9DOF");
-  Serial.println("");
-  Serial.println("");
+  // Else, continue to configuration of IMU registers:
+  configureSensor();  
+  return 0;
 }
 
-void loop() 
+////////////////////////////////////////////////////////////////////////////////
+/*
+ * End of supporting functions and declarations... BEGIN USER APP:
+ */
+////////////////////////////////////////////////////////////////////////////////
+
+void setup() {
+  cli(); // Globally disable interrupts on the AVR
+
+  get_data = 0U;
+  
+  Serial.begin(9600);
+  
+  setup_motor_control_io();
+
+  init_timer0_timebase();
+
+  drive_motors(0, DIR_REV,
+                0, DIR_FWD);
+  
+  DDRB |= (1<<0); // Turn PB0 (Digital Pin 8 on Arduino) into an OUTPUT for debug purposes
+
+  sei(); // Globally enable interrupts
+
+  init_imu();
+
+  Wire.setClock(400000L);
+}
+
+void loop() {
+    if(get_data)
+    {
+      /* Get a new sensor event */ 
+      lsm.getEvent(&accel, &mag, &gyro, &temp); 
+      get_data = 0U;
+      PORTB ^= (1<<0);
+    }
+  #ifdef RUNTIME_VERBOSE_OUTPUT
+  
+    // print out accelleration data
+    Serial.print("Accel X: "); Serial.print(accel.acceleration.x); Serial.print(" ");
+    Serial.print("  \tY: "); Serial.print(accel.acceleration.y);       Serial.print(" ");
+    Serial.print("  \tZ: "); Serial.print(accel.acceleration.z);     Serial.println("  \tm/s^2");
+  
+    // print out magnetometer data
+    Serial.print("Magn. X: "); Serial.print(mag.magnetic.x); Serial.print(" ");
+    Serial.print("  \tY: "); Serial.print(mag.magnetic.y);       Serial.print(" ");
+    Serial.print("  \tZ: "); Serial.print(mag.magnetic.z);     Serial.println("  \tgauss");
+    
+    // print out gyroscopic data
+    Serial.print("Gyro  X: "); Serial.print(gyro.gyro.x); Serial.print(" ");
+    Serial.print("  \tY: "); Serial.print(gyro.gyro.y);       Serial.print(" ");
+    Serial.print("  \tZ: "); Serial.print(gyro.gyro.z);     Serial.println("  \tdps");
+  
+    // print out temperature data
+    Serial.print("Temp: "); Serial.print(temp.temperature); Serial.println(" *C");
+  
+    Serial.println("**********************\n");
+  
+    delay(250);
+  #endif
+}
+
+ISR(TIMER2_COMPA_vect)
 {
-  lsm.read();
-
-  Serial.print("Accel X: "); Serial.print((int)lsm.accelData.x); Serial.print(" ");
-  Serial.print("Y: "); Serial.print((int)lsm.accelData.y);       Serial.print(" ");
-  Serial.print("Z: "); Serial.println((int)lsm.accelData.z);     Serial.print(" ");
-  Serial.print("Mag X: "); Serial.print((int)lsm.magData.x);     Serial.print(" ");
-  Serial.print("Y: "); Serial.print((int)lsm.magData.y);         Serial.print(" ");
-  Serial.print("Z: "); Serial.println((int)lsm.magData.z);       Serial.print(" ");
-  Serial.print("Gyro X: "); Serial.print((int)lsm.gyroData.x);   Serial.print(" ");
-  Serial.print("Y: "); Serial.print((int)lsm.gyroData.y);        Serial.print(" ");
-  Serial.print("Z: "); Serial.println((int)lsm.gyroData.z);      Serial.println(" ");
-  Serial.print("Temp: "); Serial.print((int)lsm.temperature);    Serial.println(" ");
-  delay(200);
+  get_data = 1U;
+//  PORTB ^= (1<<0);
 }
+
